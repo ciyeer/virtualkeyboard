db0a7b40f0b6e45cb1fc75210a79a77ee0a626c9
ciyeer <ciyeer@outlook.com>
2021-02-24 16:35:28 +0800

    snap
    snap

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "widget.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
+    QApplication a(argc, argv);
+    Widget w;
+    w.show();
+    return a.exec();
+}
